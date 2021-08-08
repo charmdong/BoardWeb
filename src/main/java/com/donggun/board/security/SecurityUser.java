593de9eeedc1e32b87e66ded04fffd87049ed24b
Donggun.Chung <chungdk1117@gmail.com>
2021-08-08 11:20:51 +0900

    :hammer: 2021.08.08.Sun [add comment]

--- src/main/java/com/donggun/board/security/SecurityUser.java
+++ src/main/java/com/donggun/board/security/SecurityUser.java
@@ -5,6 +5,9 @@ import org.springframework.security.core.userdetails.User;
 
 import com.donggun.board.domain.Member;
 
+/**
+ * User 클래스를 상속 받아 스프링 시큐리티에서 사용자 인증에 사용되는 UserDetails 구현체 클래스
+ */
 public class SecurityUser extends User {
 
 	private static final long serialVersionUID = 1L;
