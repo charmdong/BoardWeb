192281bc1285573d7fb4d4e5bc863afbae41bc70
Donggun.Chung <chungdk1117@gmail.com>
2021-08-29 23:28:53 +0900

    :hammer: 2021.08.29.Sun [add UserController]

---
+++ src/main/java/com/donggun/board/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.donggun.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	
+}
